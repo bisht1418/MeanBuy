ed98945c64ec1e8cc77a0093bbae2958fc236b8d
Neeraj <bishtnrj1418@gmail.com>
2022-11-11 21:44:51 +0530

    Nav bar, Fotter and body completed

---
+++ js/script.js
@@ -0,0 +1,59 @@
+   
+    let myDiv = document.querySelector("#container")
+    let noMultiData = JSON.parse(localStorage.getItem("cart"))
+    let cart = JSON.parse(localStorage.getItem("cart"))||[]
+    let bag = []
+
+    let url = "https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-tech-products"
+
+    fetch(url).then((res)=>res.json())
+              .then((data)=>{
+                let x = data.data
+                bag = x
+                console.log(bag)
+                displayData(x)
+              })
+
+
+           
+
+    function displayData(data){
+
+        myDiv.innerHTML = null
+
+        data.forEach(function(element){
+
+        let div = document.createElement("div")
+
+        let img = document.createElement("img")
+        img.setAttribute("src",element.img)
+
+        let brand = document.createElement("h2")
+        brand.innerText = element.brand
+
+        let price = document.createElement("h3")
+        price.innerText = element.price
+
+        let detail = document.createElement("p")
+        detail.innerText = element.details
+
+        let Category= document.createElement("p")
+        Category.innerText = element.category
+
+        let button = document.createElement("button")
+        button.innerText = "Add to Cart"
+        button.addEventListener("click", ()=>{
+
+         
+            cart.push(element)
+            localStorage.setItem("cart",JSON.stringify(cart))
+        })
+
+        div.append(img,brand,price,detail,Category,button)
+        myDiv.append(div)
+
+      })
+
+    }  
+  
+   
